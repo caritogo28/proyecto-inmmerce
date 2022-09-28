ff26de596235c3a79c45f18855c4779c54d51257
Carolina Gomez <caritogo28@gmail.com>
2022-09-27 19:09:05 -0500

    Header y footer

---
+++ react/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => (
+    <div>
+        <h1 className="tc">¡Gracias por visitar la tienda de Carolina!</h1>
+    </div>
+);
+
+HelloWorld.displayName = "helloworld";
+
+export default HelloWorld;
